e7850263b84413e1baaf94529e1c9180762e0382
Mohd Asif S Siddiqui <mohammadasifshahid@gmail.com>
2025-09-04 22:47:40 +0530

    update

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,15 @@
 import {
-    Inngest,
+    inngest,
     syncUserCreation,
     syncUserDeletion,
     syncUserUpdation,
 } from "@/config/inngest";
+
 import {serve} from "inngest/next";
 
-// Create a client to send and receive events
-export const inngest = new Inngest({id: ""});
-
-// Create an API that serves zero functions
-export default serve({
-    client: inngest,
-    functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
-});
+// Serve API with Inngest client + functions
+export const {GET, POST, PUT} = serve(inngest, [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+]);
